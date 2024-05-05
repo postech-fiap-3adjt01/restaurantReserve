f7db57345e7bf928b4e6c9f2d78610b14902be3f
giullianamunhoz <giullianamunhoz9@gmail.com>
2024-05-05 18:35:14 -0300

    Teste docker

---
+++ src/main/java/com/fiap/restaurante/Controller/RestauranteController.java
@@ -0,0 +1,12 @@
+package com.fiap.restaurante.Controller;
+
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RestauranteController {
+
+    public findAll()          {
+        return "all"   }
+}
